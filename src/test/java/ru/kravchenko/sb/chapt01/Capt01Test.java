f25b2123f9953458331322ad25afb3e11ae1fb18
Кравченко Роман Владимирович (4061571) <rvkravchenko@vtb.ru>
2022-11-29 16:33:20 +0300

    Initial commit

---
+++ src/test/java/ru/kravchenko/sb/chapt01/Capt01Test.java
@@ -0,0 +1,13 @@
+
+package ru.kravchenko.sb.chapt01;
+
+import org.junit.jupiter.api.Test;
+
+public class Capt01Test {
+
+    @Test
+    public void test01(){
+        System.out.println("lll");
+    }
+
+}
